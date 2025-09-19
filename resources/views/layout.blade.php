c91b5102e184fede7954f4f62f79508797b036f6
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-19 15:58:19 +0200

    Create: hero-section component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,12 +3,16 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
+   <link rel="stylesheet" href="{{asset('css/style.css')}}">
    @vite('resources/css/app.css')
    <title>{{ $title ?? 'JobSea | Catch your dream job' }}</title>
 </head>
 <body class="bg-gray-100">
    <x-header />
+   @if (request()->is('/'))
+   <x-hero-section/>
+   @endif
    <main class="container mx-auto p-4 mt-4">
     {{ $slot }}
    </main>
