b87fd35f6113dbb4e9c34464daedf09f61207a09
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-19 09:00:14 +0200

    Style: mobile menu

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -3,10 +3,11 @@
     'icon' => null,
     'bgClass' => 'bg-green-500',
     'hoverClass' => 'hover:bg-green-400',
-    'textClass' => 'text-black'
+    'textClass' => 'text-black',
+    'block' => false,
 ])
 
-<a href="{{$url}}" class="{{$bgClass}} {{$hoverClass}} {{$textClass}} px-4 py-2 rounded hover:shadow-md transition duration-300">
+<a href="{{$url}}" class="{{$bgClass}} {{$hoverClass}} {{$textClass}} {{$block ? 'block' : ''}} px-4 py-2 rounded hover:shadow-md transition duration-300">
     @if($icon)
     <i class="fa fa-{{$icon}}"></i>
     @endif
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,6 +3,8 @@
         <h1 class="text-3xl font-semibold">
             <a href="{{url('/')}}">JobSea</a>
         </h1>
+
+        <!-- Desktop Menu -->
         <nav class="hidden md:flex items-center space-x-4">
             <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
             <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
@@ -16,14 +18,14 @@
             <i class="fa fa-bars text-2xl"></i>
         </button>
     </div>
+
     <!-- Mobile Menu -->
-    <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
-        <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
-        <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
-        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
-        <x-nav-link url="/login" :active="request()->is('login')" icon="user">Login</x-nav-link>
-        <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
-        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-        <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+    <nav id="mobile-menu" class="hidden sm:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true" icon="user">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true" icon="gauge">Dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" :block="true" icon="edit">Create Job</x-button-link>
     </nav>
 </header>
