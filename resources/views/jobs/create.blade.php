34cf627f1bc06007a25c4178d9fb1a0cbe81f8f5
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-23 17:55:40 +0200

    Add: input data validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,8 +3,18 @@
    <h2>Create a new job</h2>
    <form action="/jobs" method="POST" class="my-2">
       @csrf
-      <input type="text" name="title" placeholder="title" class="p-2 bg-white">
-      <input type="text" name="description" placeholder="description" class="p-2 bg-white">
+      <div class="mt-2">
+         <input type="text" name="title" placeholder="title" value="{{ old('title') }}" class="p-2 bg-white">
+         @error('title')
+            <div class="mb-2 text-sm text-red-500">{{ $message }}</div>
+         @enderror
+      </div>
+      <div class="my-2">
+         <input type="text" name="description" placeholder="description" value="{{ old('description') }}" class="p-2 bg-white">
+         @error('description')
+            <div class="mb-2 text-sm text-red-500">{{ $message }}</div>
+         @enderror
+      </div>
       <button type="submit" class="p-2 rounded-md bg-blue-500 text-white">Submit</button>
    </form>
 </x-layout >
