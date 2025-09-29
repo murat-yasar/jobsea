6c34848b6c290233260a54e12f12392e9b2e1689
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-29 16:07:29 +0200

    Add: jobCard component

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,12 +1,11 @@
 <x-layout >
    <x-slot name="title">Available Jobs</x-slot>
-   <h2>Available Jobs</h2>
 
-   <ul>
+   <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
       @forelse ($jobs as $job)
-      <li><a href="{{ route('jobs.show', $job->id) }}">{{ $job->title }}</a> - {{ $job->description }}</li>
+      <x-job-card :job="$job" />
       @empty
       <p>No jobs available!</p>
       @endforelse
-   </ul>
+   </div>
 </x-layout >
