a90ed465dff2e16d63681b62bd87b5d34c9f7905
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-29 16:21:44 +0200

    Style: homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use Illuminate\Http\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
