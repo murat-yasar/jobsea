7d5e58ae33ebb610e2acd7700177a91e4b1cbe61
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-06 14:05:24 +0200

    Refactore: alert and hamburger with alpineJS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
    <link rel="stylesheet" href="{{asset('css/style.css')}}">
    @vite('resources/css/app.css')
+   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
    <title>{{ $title ?? 'JobSea | Catch your dream job' }}</title>
 </head>
 <body class="bg-gray-100">
