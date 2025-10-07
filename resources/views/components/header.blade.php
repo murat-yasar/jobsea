7487e618eb568c445b72780e4ac91684fc3fe448
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-07 23:10:20 +0200

    Fix: hidden hamburger menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -24,13 +24,14 @@
     </div>
 
     <!-- Mobile Menu -->
-    <nav id="mobile-menu" x-show="open" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+    <nav id="mobile-menu" x-show="open" @click.outside="open = false" class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         @auth
         <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
         <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true" icon="gauge">Dashboard</x-nav-link>
-        <x-logout-button />
-        <div class="pt-2"></div>
+        <div class="pt-2 px-4">
+            <x-logout-button />
+        </div>
         <x-button-link url="/jobs/create" :block="true" icon="edit">Create Job</x-button-link>
         @else
         <x-nav-link url="/login" :active="request()->is('login')" :mobile="true" icon="user">Login</x-nav-link>
