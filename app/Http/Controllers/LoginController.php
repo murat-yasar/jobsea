02cc678f8ae4adaa77de2f5e6ff3206a6839081d
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-07 21:42:20 +0200

    Add: login controller and blade

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,5 +13,25 @@ class LoginController extends Controller
     // @route GET /login
     public function login(): View {
         return View('auth.login');
+    }
+
+    // @desc Authenticate user
+    // @route POST /login
+    public function authenticate(Request $request): RedirectResponse {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // Attempt to authenticate user
+        if(Auth::attempt($credentials)){
+            // Regenerate the session to prevent fixation attacs
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'))->with('success', 'You are now logged in!');
+        }
+
+        // If auth fails, redirect back with error
+        return back()->withErrors(['email'=>'The entered credentials are not matching!'])->onlyInput('email');
     }
 }
