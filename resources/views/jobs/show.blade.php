551134d81e5fe03c1b3b04ee1ac184869a8ce80c
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-07 17:33:14 +0200

    Fix: syntax

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -10,16 +10,12 @@
                </a>
                <div class="flex space-x-3 ml-4">
                   <!-- Edit Button -->
-                  <a href="{{ route('jobs.edit', $job->id) }}"
-                     class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
-                  >Edit</a>
+                  <a href="{{ route('jobs.edit', $job->id) }}" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
                   <!-- Delete Form -->
                   <form method="POST" action="{{ route('jobs.destroy', $job->id) }}" onsubmit="return confirm('Are you sure about to delete this job listing?')">
                      @csrf
                      @method('DELETE')
-                     <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">
-                        Delete
-                     </button>
+                     <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">Delete</button>
                   </form>
                   <!-- End Delete Form -->
                </div>
