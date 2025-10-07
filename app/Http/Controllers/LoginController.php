3de13fb0fd90863184f5959b00e93acddd69ff1c
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-07 18:07:30 +0200

    Add: login and register controllers

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class LoginController extends Controller
+{
+    // @desc Show login form
+    // @route GET /login
+    public function login(): View {
+        return View('auth.login');
+    }
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class RegisterController extends Controller
+{
+    // @desc Show register form
+    // @route GET /register
+    public function register(): View {
+        return View('auth.register');
+    }
+}
