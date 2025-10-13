a6b0b4eed687994722ce336c4a6bb0ef17aff4cc
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-13 10:07:45 +0200

    Add: pagination

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -18,7 +18,7 @@ class JobController extends Controller
     // @route GET /jobs
     public function index(): View
     {
-        $jobs = Job::all();
+        $jobs = Job::paginate(6);
         return view('jobs.index')->with('jobs', $jobs);
     }
 
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,4 +8,6 @@
       <p>No jobs available!</p>
       @endforelse
    </div>
+   {{-- Pagination Links --}}
+   {{ $jobs->links() }}
 </x-layout >
