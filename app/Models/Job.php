5e1974567c7c7490ea736b2f78116135a36a4563
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-13 19:56:51 +0200

    Add: Bookmarks

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
@@ -35,7 +36,14 @@ class Job extends Model
    ];
 
    // Relation to user
-   public function user():BelongsTo {
+   public function user():BelongsTo
+   {
       return $this->belongsTo(User::class);
+   }
+
+   // Relation to bookmarks
+   public function bookmarkedByUsers(): BelongsToMany
+   {
+      return $this->belongsToMany(User::class, 'Job_user_bookmarks')->withTimestamps();
    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,5 +51,11 @@ class User extends Authenticatable
     // Relate to job listings
     public function jobListings(): HasMany {
         return $this->hasMany(JobListing::class);
+    }
+
+    // Relation to bookmarks
+    public function bookmarkedJobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
