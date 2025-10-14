fe36cc5548a782b6e4fb2ad6ba1ecfb75cd41f66
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-14 19:07:19 +0200

    Feat: prevent multiple application of a user to a single job

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -13,6 +13,13 @@ class ApplicantController extends Controller
     // @route   POST /jobs/{job}/apply
     public function store(Request $request, Job $job): RedirectResponse
     {
+        // Check if the user has already applied
+        $existingApplication = Applicant::where('job_id', $job->id)->where('user_id', auth()->id())->exists();
+
+        if($existingApplication){
+            return redirect()->back()->with('error', 'You have already applied to this job!');
+        }
+
         // Validate the incoming request data
         $validatedData = $request->validate([
             'full_name' => 'required|string|max:255',
