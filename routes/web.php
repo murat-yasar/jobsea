815c37ae42d66cd8e86f2b16a1a46ed721e1774d
Murat Yaşar <dev.muratyasar@gmail.com>
2025-10-15 08:16:49 +0200

    Hide API key from the users

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\ApplicantController;
+use App\Http\Controllers\GeocodeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -42,3 +43,5 @@ Route::middleware('auth')->group(function ()
 Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])->name('applicant.store')->middleware('auth');
 
 Route::delete('/applicants/{applicant}', [ApplicantController::class, 'destroy'])->name('applicants.destroy')->middleware('auth');
+
+Route::get('/geocode', [GeocodeController::class, 'geocode']);
